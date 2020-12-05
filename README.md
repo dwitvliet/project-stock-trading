62f09debaf816e86763b4a771271b57c518008b6
witvliet <daniel.witvliet@gmail.com>
2020-12-04 23:20:18 -0500

    Added script to setup database

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # project-stock-trading
 Sample project for collecting and predicting stock prices
 
-## Requirements
-Python3
-MySQL server
-Python modules in `requirements.txt`
+## Setup
+
+Install MySQL.
+
+Install dependencies.
+   
+    python3 -m pip install requirements.txt
+
+Setup database: 
+
+    database/setup.sh database_name username password polygon_api_key | sudo -u root mysql
---
+++ data_collection/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -ne 4 ]
+then
+    echo "Usage: $0 <DATABASE-NAME> <DATABASE-USER> <DATABASE-PASWORD> <API-KEY>"
+    exit 1
+fi
+
+DATABASE="$1"
+DATABASE_USER="$2"
+DATABASE_PASSWORD="$3"
+API_KEY="$4"
+
+cat > config.py << EOF
+database = {
+  'name': '$DATABASE',
+  'user': '$DATABASE_USER',
+  'password': '$DATABASE_PASSWORD',
+}
+
+api_key = '$API_KEY'
+EOF
+
+cat <<EOSQL
+CREATE DATABASE IF NOT EXISTS $DATABASE;
+CREATE USER IF NOT EXISTS '$DATABASE_USER'@'localhost' IDENTIFIED BY '$DATABASE_PASSWORD';
+GRANT ALL PRIVILEGES ON $DATABASE.* to '$DATABASE_USER'@'localhost';
+EOSQL
+
