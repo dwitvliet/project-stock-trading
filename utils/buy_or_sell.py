1bbfee9e3662edb655a4e04d862fe557a15571f8
witvliet <daniel.witvliet@gmail.com>
2020-12-17 21:41:49 -0500

    Fixed potential bug where last second of price increases where not counted towards profits

--- utils/buy_or_sell.py
+++ utils/buy_or_sell.py
@@ -86,18 +86,17 @@ def label_timeseries(bars, gain_threshold):
     return label
 
 
-def profits(bars, price='price', prediction='prediction', buy_cost=0):
+def profits(bars, buy_or_sell, buy_cost=0):
     """ Test how profitable predictions are.
     
     Given a dataframe of prices and a prediction on whether to buy or sell at a
     certain time, calculate the profits gained during the time period.
     
     Args:
-        bars (pd.DataFrame): Dataframe with each row being a time period.
-        price (str, optional): The column containing prices.
-        prediction (str, optional): The column containing predictions on whether
-            to buy or not. Values should be either 1 (buy), 0 (keep), or
-            -1 (sell).
+        bars (pd.Series): Time series with prices.
+        buy_or_sell (pd.Series): Time series containing predictions on whether
+            to buy or not. Values should be either 1 (buy), 0 (keep), or -1
+            (sell).
         buy_cost (float, optional): The cost of buying and selling a stock, to
             be substracted from the profits.
             
@@ -108,21 +107,22 @@ def profits(bars, price='price', prediction='prediction', buy_cost=0):
 
     # Determine which time periods the stock is owned.
     own = pd.Series(index=bars.index)
-    own[bars[prediction].isin((1, 'buy'))] = True
-    own[bars[prediction].isin((-1, 'sell'))] = False
+    own[buy_or_sell.isin((1, 'buy'))] = True
+    own[buy_or_sell.isin((-1, 'sell'))] = False
     own = own.fillna(method='ffill').fillna(False)
 
     # Determine relative gain during owned epochs.
-    price_by_epoch = bars[price][own].groupby(
-        (own.shift() != own).cumsum()[own]
-    )
-    price_at_buy = price_by_epoch.head(1).values
-    price_at_sell = price_by_epoch.tail(1).values
-    gains_per_epoch = (price_at_sell - price_at_buy - buy_cost) / price_at_buy
+    bars = bars.rename('price').to_frame()
+    bars['increase'] = bars.shift(-1) - bars
+    bars['increase'] = bars['increase'].fillna(0)
+    bars_by_epoch = bars[own].groupby((own.shift() != own).cumsum()[own])
+    price_at_buy = bars_by_epoch['price'].head(1).values
+    gain_after_sell = bars_by_epoch['increase'].sum()
+    gains_per_epoch = (gain_after_sell - buy_cost) / price_at_buy
 
-    # Print result.
+    # Calculate and return result.
     active_gain = np.prod(1 + gains_per_epoch) - 1
-    passive_gain = bars[price].dropna()[-1] / bars[price].dropna()[0] - 1
+    passive_gain = bars['price'].dropna()[-1] / bars['price'].dropna()[0] - 1
 
     return {
         'active_gain': active_gain,
