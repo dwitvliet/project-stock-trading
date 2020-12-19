955ff56450b57b404c9867b625973bcf81782d10
witvliet <daniel.witvliet@gmail.com>
2020-12-19 00:24:42 -0500

    Added feature manager to keep track of all features

--- features/feature_manager.py
+++ features/feature_manager.py
@@ -1,8 +1,28 @@
+import logging
+
+import pandas as pd
+
+import data.data_manager as data
+
+
 class FeatureManager:
 
     def __init__(self, ticker):
         self.ticker = ticker
-        self.features = []
+        self.features = {}
 
-    def new(self, *args):
-        self.features.append(args)
+    def new(self, name=None, func=None, desc=None, params=None):
+        assert name is not None and func is not None, (
+            'A name and function is required for a feature.'
+        )
+        assert name not in self.features, (
+            f'Feature `{name}` ({self.ticker}) has already been registered.'
+        )
+        if params is None:
+            params = {}
+        self.features[name] = {
+            'name': name,
+            'func': func,
+            'desc': desc,
+            'params': params,
+        }
