4d03c62eff1cd2fe673462e38e6ac59f44d8bf17
witvliet <daniel.witvliet@gmail.com>
2021-01-08 15:30:46 -0500

    Added utility feature to serialize dict

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,7 @@ def timer():
     """ Time the execution of a context block.
 
     Yields:
-      None
+        None
     """
     time_start = time.time()
 
@@ -15,3 +15,17 @@ def timer():
 
     elapsed = time.time() - time_start
     print(f'Time: {elapsed:.2f}s')
+
+
+def serialize_dict(dict_):
+    """ Serialize dict to str, sorting keys.
+
+    Args:
+        dict_: Dict to sort.
+
+    Returns:
+        str
+
+    """
+
+    return '_'.join([f'{k}_{v}' for k, v in sorted(dict_.items())])
